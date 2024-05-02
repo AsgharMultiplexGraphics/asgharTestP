9ae041edd54a0def7db3b93980d77cdc4fd4ad7e
Asghar Ali <asghar.ssri.jc@gmail.com>
2024-05-02 18:11:57 +0500

    wip on nav

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+    def nav_classes
+        return "navbar navbar-expand-lg navbar-dark bg-dark fixed-top mb-20" if !user_signed_in?
+
+        if current_user.cashier?
+         "navbar navbar-expand-lg navbar-dark bg-primary fixed-top mb-10"
+        elsif current_user.manager?
+         "navbar navbar-expand-lg navbar-dark bg-success fixed-top mb-10"
+        else 
+         "navbar navbar-expand-lg navbar-dark bg-dark fixed-top mb-10"
+        end             
+    end
+
 end
